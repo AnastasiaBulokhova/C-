9634cdcf11ca5aa67070aa1f6f4aa34e079a2506
Anastasia <anastbulokhova@gmal.com>
2023-08-31 22:10:02 +0300

    Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int Xa = 40, Ya = 1,
+    Xb = 1, Yb = 30,
+    Xc = 80, Yc = 30;
+
+Console.SetCursorPosition(Xa, Ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(Xb, Yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(Xc, Yc);
+Console.WriteLine("+");
+
+int x = Xa;
+int y = Xb;
+
+int count = 0;
+
+while (count < 50000)
+{
+    int what = new Random().Next(0, 3); //[0;3) 0 1 2
+    if (what == 0)
+    {
+        x = (x + Xa) / 2;
+        y = (y + Ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + Xb) / 2;
+        y = (y + Yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + Xc) / 2;
+        y = (y + Yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count += 1;
+}
